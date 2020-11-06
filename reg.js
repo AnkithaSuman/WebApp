e2989bc2030f9a7e5f7e5ad9bf2b6a5e555d7be6
Ankitha Suman <19ankithasuman@gmail.com>
2020-11-06 18:57:14 +0530

    New files are committed

---
+++ reg.js
@@ -0,0 +1,6 @@
+myFunc = function() {
+    var first = document.getElementById("fName").value;
+    var second = document.getElementById("sName").value;
+
+    document.getElementById("here").innerHTML = first+" "+second;
+}
